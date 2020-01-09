397ad70820ff9efbcfe79190a84caf2c2ab75e0d
github username <github email address>
2020-01-08 21:08:44 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
+  
 
   login = ({ username, password }) => {
     console.log(`Logging in ${username} with password ${password}`);
@@ -16,7 +17,6 @@ class App extends Component {
           <pre>LoginForm</pre>
         </h1>
         <LoginForm handleLogin={this.login} />
-
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      counter: this.props.maxChars,
+      message: ""
+    };
+  }
+
+  
+
+  handleChange = (event) => {
+
+    this.setState({
+      counter: this.props.maxChars - event.target.value.length ,
+      message:event.target.value
+    }) 
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleChange} type="text" name="message" id="message" value={this.state.message} />
+        <h4>{this.state.counter}</h4>
       </div>
     );
   }
